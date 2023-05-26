7ac1f1bff496eeef217f60ba6d18dc04dfeae65a
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-05-26 15:02:59 +0900

    날짜/시간 예약 혼자 해보기

---
+++ 5월 4주차/Self6_1/MainActivity.java
@@ -0,0 +1,111 @@
+package com.kh.self6_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Chronometer chronometer;
+    RadioButton rdDate, rdTime;
+    DatePicker datePicker;
+    TimePicker timePicker;
+    TextView txtResult;
+    LinearLayout layout;
+    int year, month, day;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("시간 예약");
+        setUI();
+        setListener();
+        initDate();
+    }
+
+    private void initDate() {
+        Calendar cal = Calendar.getInstance();
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH) + 1;
+        day = cal.get(Calendar.DAY_OF_MONTH);
+    }
+
+    private void setListener() {
+        rdDate.setOnClickListener(this);
+        rdTime.setOnClickListener(this);
+        chronometer.setOnClickListener(this);
+        txtResult.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                chronometer.stop();
+                layout.setVisibility(View.INVISIBLE);
+                txtResult.setText(setDate() + setTime() + "예약됨");
+                rdDate.setChecked(false);
+                rdTime.setChecked(false);
+                chronometer.setTextColor(Color.rgb(31, 97, 141 ));
+                return false;
+            }
+        });
+        datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                MainActivity.this.year = year;
+                MainActivity.this.month = month + 1;
+                MainActivity.this.day = day;
+            }
+        });
+    }
+
+    private void setUI() {
+        chronometer = findViewById(R.id.chronometer);
+        rdDate = findViewById(R.id.rdDate);
+        rdTime = findViewById(R.id.rdTime);
+        datePicker = findViewById(R.id.datePicker);
+        timePicker = findViewById(R.id.timePicker);
+        txtResult = findViewById(R.id.txtResult);
+        layout = findViewById(R.id.layout);
+    }
+
+    private String make2digits(int num) {
+        String result = num < 10 ? "0" + num : num + "";
+        return result;
+    }
+
+    private String setDate() {
+        return make2digits(year) + "년 " + make2digits(month) + "월 " + make2digits(day) + "일 ";
+    }
+
+    private String setTime() {
+        int hour = timePicker.getHour();
+        int min = timePicker.getMinute();
+        return make2digits(hour) + "시 " + make2digits(min) + "분 ";
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == chronometer) {
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            chronometer.start();
+            layout.setVisibility(View.VISIBLE);
+            chronometer.setTextColor(Color.rgb(192, 57, 43));
+        } else if (view == rdDate) {
+            datePicker.setVisibility(View.VISIBLE);
+            timePicker.setVisibility(View.INVISIBLE);
+        } else if (view == rdTime) {
+            datePicker.setVisibility(View.INVISIBLE);
+            timePicker.setVisibility(View.VISIBLE);
+        }
+    }
+}
