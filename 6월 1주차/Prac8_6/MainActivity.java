233df5df948804421a019c0e8ba13a1d15042d03
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-06-01 17:35:03 +0900

    간단다이어리 - sd카드에 저장/읽기

---
+++ 6월 1주차/Prac8_6/MainActivity.java
@@ -0,0 +1,119 @@
+package com.kh.prac8_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
+    EditText text;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this,
+                new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
+
+        datePicker = findViewById(R.id.datePicker);
+        text = findViewById(R.id.text);
+        button = findViewById(R.id.button);
+
+        setListener();
+    }
+
+    private void setListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int year = datePicker.getYear();
+                int month = datePicker.getMonth();
+                int day = datePicker.getDayOfMonth();
+
+                String filename = year + "_" + (month + 1) + "_" + day + ".txt";
+
+
+                button.setText("save");
+
+//                try {
+//                    FileOutputStream fos = openFileOutput(filename, MODE_PRIVATE);
+//                    String str = text.getText().toString();
+//                    byte[] bytes = str.getBytes();
+//                    fos.write(bytes);
+//                    Toast.makeText(MainActivity.this, "저장 완료", Toast.LENGTH_SHORT).show();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+                try {
+                    File file = new File(("/sdcard/" + filename));
+                    FileOutputStream fos = new FileOutputStream(file);
+                    String str = text.getText().toString();
+                    fos.write(str.getBytes());
+                    Log.d("mytag",text.getText().toString());
+                    Toast.makeText(MainActivity.this, "저장 완료", Toast.LENGTH_SHORT).show();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                String filename = year + "_" + (month + 1) + "_" + day + ".txt";
+
+//                try {
+//                    FileInputStream fis = openFileInput(filename);
+//                    BufferedInputStream bis = new BufferedInputStream(fis);
+//                    byte[] bytes = new byte[300];
+//                    bis.read(bytes);
+//                    text.setText(new String(bytes));
+//                    bis.close();
+//                    Toast.makeText(MainActivity.this, "로딩 완료", Toast.LENGTH_SHORT).show();
+//                    button.setText("update");
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    button.setText("save");
+//                } // 실행 제대로 안됨
+                    try {
+                        File file = new File(("/sdcard/"+filename));
+                        FileInputStream fis = new FileInputStream(file);
+                        byte[] bytes = new byte[300];
+                        fis.read(bytes);
+                        text.setText(new String(bytes));
+                        fis.close();
+                        button.setText("update");
+                        return;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        button.setText("save");
+                        text.setText("");
+                    }
+
+            }
+        });
+
+    } //setListener()
+}
