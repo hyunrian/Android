5de481c36fa77f4338d3fa44bff77fb027778e56
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-06-05 16:21:07 +0900

    명화 선호도 투표 앱-결과창 업그레이드 (Activity와 Comparator 활용)

--- 6월 2주차/Project10_2/ResultActivity.java
+++ 6월 2주차/Project10_2/ResultActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -22,6 +32,14 @@ public class ResultActivity extends AppCompatActivity {
     };
     TextView[] textViews = new TextView[tvIds.length];
     RatingBar[] ratingBars = new RatingBar[rbIds.length];
+
+    int imgIds[] = new int[tvIds.length];
+
+    TextView tvTop;
+    ImageView imgTop;
+
+//    HashMap<Integer, Integer> map = new HashMap<>();
+    ArrayList<ImageSet> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +66,57 @@ public class ResultActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+                list.clear();
             }
         });
 
+        imgTop = findViewById(R.id.imgTop);
+        tvTop = findViewById(R.id.tvTop);
+
+        for (int i = 0; i < counts.length; i++) {
+            list.add(new ImageSet(imgNames[i], counts[i]));
+        }
+        Collections.sort(list, new ImageCountComparator());
+        Log.d("mytag", list.toString());
+
+        tvTop.setText(list.get(0).name);
+        for (int i = 0; i < textViews.length; i++) {
+            if (imgNames[i].equals(tvTop.getText())) {
+                int id = getResources().getIdentifier("pic" + (i+1), "drawable", getPackageName());
+                imgTop.setImageResource(id);
+                break;
+            }
+        }
     }
+
+    private class ImageSet {
+        int count;
+        String name;
+
+        ImageSet(String name, int count) {
+            this.name = name;
+            this.count = count;
+        }
+    }
+
+    private class ImageCountComparator implements Comparator<ImageSet> {
+
+        @Override
+        public int compare(ImageSet is1, ImageSet is2) {
+            if (is1.count > is2.count) // reverse를 하지 않고 기본 정렬에서 내림차순하려면 -1 리턴
+                return -1;
+            else if (is1.count < is2.count)
+                return 1;
+            else
+//                Collections.sort(list, new ImageNameComparator().reversed()); // 이런 식의 n차 정렬은 되지 않음
+                return is1.name.compareTo(is2.name);
+        }
+    }
+//    private class ImageNameComparator implements Comparator<ImageSet> {
+//
+//        @Override
+//        public int compare(ImageSet is1, ImageSet is2) {
+//            return is1.name.compareTo(is2.name);
+//        }
+//    }
 }
